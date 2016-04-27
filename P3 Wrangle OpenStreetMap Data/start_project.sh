f73b01a810c7cdaf657bce19ae3fc376f070b05e
Tom Hrabchak <thrabchak@gmail.com>
2016-04-27 08:02:55 -0400

    Added start_project.sh which launches useful apps for working on the project

---
+++ P3 Wrangle OpenStreetMap Data/start_project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mongod &
+jupyter notebook &
+chromium https://github.com/thrabchak/Udacity-Data-Analysis/tree/master/P3%20Wrangle%20OpenStreetMap%20Data &
+chromium https://docs.google.com/document/d/1TpfNxDzUjhibq9Qb8cOQHtlvZUelft-W0fb7pCTTyYE/pub & 
+chromium https://classroom.udacity.com/nanodegrees/nd002/parts/0021345404/modules/316820862075463/lessons/3168208620239847/concepts/77135319070923 &
+
