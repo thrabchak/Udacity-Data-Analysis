61de5df9f9a22d54e606a6862b6f0e302067163d
Tom Hrabchak <thrabchak@gmail.com>
2016-05-10 08:37:18 -0400

    transform_data doesn't get hung up on missing ref attributes on node_refs elements

--- P3 Wrangle OpenStreetMap Data/transform_data.py
+++ P3 Wrangle OpenStreetMap Data/transform_data.py
@@ -148,7 +148,8 @@ def shape_element(element):
         if(len(noderefs) > 0):
             node["node_refs"] = []
             for nr in noderefs:
-                node["node_refs"].append(nr.attrib['ref'])
+                if 'ref' in nr.attrib.keys():
+                    node["node_refs"].append(nr.attrib['ref'])
 
         return node
     else:
@@ -156,7 +157,7 @@ def shape_element(element):
 
 
 def process_map(file_in, pretty = False):
-    file_out = "{0}.json".format(file_in)
+    file_out = "{0}.json".format(file_in[:-4])
     data = []
     with codecs.open(file_out, "w") as fo:
         context = ET.iterparse(file_in, events=("start", "end"))
@@ -175,34 +176,6 @@ def process_map(file_in, pretty = False):
 
 def main():
     process_map("data.xml")
-
-def test():
-    # NOTE: if you are running this code on your computer, with a larger dataset, 
-    # call the process_map procedure with pretty=False. The pretty=True option adds 
-    # additional spaces to the output, making it significantly larger.
-    data = process_map('example2.osm', True)
-    pprint.pprint(data[-1])
-    
-    correct_first_elem = {
-        "id": "261114295", 
-        "visible": "true", 
-        "type": "node", 
-        "pos": [41.9730791, -87.6866303], 
-        "created": {
-            "changeset": "11129782", 
-            "user": "bbmiller", 
-            "version": "7", 
-            "uid": "451048", 
-            "timestamp": "2012-03-28T18:31:23Z"
-        }
-    }
-    assert data[0] == correct_first_elem
-    assert data[-1]["address"] == {
-                                    "street": "West Lexington St.", 
-                                    "housenumber": "1412"
-                                      }
-    assert data[-1]["node_refs"] == [ "2199822281", "2199822390",  "2199822392", "2199822369", 
-                                    "2199822370", "2199822284", "2199822281"]
 
 if __name__ == "__main__":
     main()
